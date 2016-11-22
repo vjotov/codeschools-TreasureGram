32a779bfff85b8163d481c6320c2e09cc7202360
VladimirJotov <vjoto@saritasa.com>
2016-11-22 17:33:04 +0700

    improving URL dispacher

--- Treasuregram/urls.py
+++ Treasuregram/urls.py
@@ -13,12 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
-from main_app import views
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    # localhost/index
-    url(r'^index/', views.index),
+    # localhost:8000 will go here
+    url(r'^', include('main_app.urls')),
 ]
