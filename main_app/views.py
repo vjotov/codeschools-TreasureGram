02fe723f130baf118056dc19a950eed0f30a81f4
VladimirJotov <vjoto@saritasa.com>
2016-11-23 12:27:09 +0700

    treasure collection

--- main_app/views.py
+++ main_app/views.py
@@ -2,14 +2,22 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
-    name = 'Gold Nugget'
-    value = 1000.00
-    context = {'treasure_name': name,'treasure_val': value }
-    
-    return render(request, 'index.html', context)
+def index(request):  
+    return render(request, 'index.html', {'treasures': treasures})
 
 def home(request):
     return HttpResponse('Welcome Home Travelers!')
     
-    
+
+class Treasure:
+    def __init__(self, name, value, material, location):
+        self.name = name
+        self.value = value
+        self.material = material
+        self.location = location
+
+treasures = [
+    Treasure('Gold Nugget', 500.00, 'gold', "Curly's Creek, NM"),
+    Treasure("Fool's Gold", 0, 'pyrite', "Fool's Falls, CO"),
+    Treasure('Coffee Can', 20.00, 'tin', 'Aacme, CA'),
+]
