003a47f9464bc207e83e1862cb4ad681445df3fb
VladimirJotov <vjoto@saritasa.com>
2016-11-24 17:26:13 +0700

    challenges part 2.2

--- main_app/views.py
+++ main_app/views.py
@@ -1,15 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def index(request):  
     return render(request, 'index.html', {'treasures': treasures})
 
 def home(request):
-    location_name = "Fool's Falls"
-    predators = 'Scorpions, Snakes'
-    context = {'location_name': location_name,
-               'predators': predators}
+    context = {'locations': locations}
     return render(request, 'home.html', context)
     
 
@@ -21,8 +17,24 @@ class Treasure:
         self.location = location
         self.img_url = img_url
 
+class Location:
+    def __init__(self, name, predators, num_restaurants, img_url):
+        self.name = name
+        self.predators = predators
+        self.num_restaurants = num_restaurants
+        self.img_url = img_url
+
 treasures = [
     Treasure('Gold Nugget', 500.00, 'gold', "Curly's Creek, NM",'/static/images/nugget.png'),
     Treasure("Fool's Gold", 0, 'pyrite', "Fool's Falls, CO",'/static/images/fools-gold.png'),
     Treasure('Coffee Can', 20.00, 'tin', 'Aacme, CA','/static/images/coffee-can.png'),
+]
+
+locations = [
+    Location("Fool's Falls, CO", 'Flash Floods', 0,
+             'http://courseware.codeschool.com/try_django/images/fools-falls.jpg'),
+    Location("Curly's Creek, NM", 'Rattle Snakes', 2,
+             'http://courseware.codeschool.com/try_django/images/curlys-creek.jpg'),
+    Location("The Delicate Arch, UT", "str", 5, 
+             "http://courseware.codeschool.com/try_django/images/delicate-arch.jpg"),
 ]
