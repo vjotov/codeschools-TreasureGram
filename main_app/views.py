5763d40122842ebec57114f89ceb91f1f450e685
Vladimir Jotov <vjotov@>
2016-11-25 17:57:16 +0700

    adding models

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from .models import Treasure
 
 # Create your views here.
 def index(request):  
+	treasures = Treasure.objects.all()
     return render(request, 'index.html', {'treasures': treasures})
 
 def home(request):
@@ -9,13 +11,13 @@ def home(request):
     return render(request, 'home.html', context)
     
 
-class Treasure:
-    def __init__(self, name, value, material, location, img_url):
-        self.name = name
-        self.value = value
-        self.material = material
-        self.location = location
-        self.img_url = img_url
+#class Treasure:
+#    def __init__(self, name, value, material, location, img_url):
+#        self.name = name
+#        self.value = value
+#        self.material = material
+#        self.location = location
+#        self.img_url = img_url
 
 class Location:
     def __init__(self, name, predators, num_restaurants, img_url):
@@ -24,11 +26,11 @@ class Location:
         self.num_restaurants = num_restaurants
         self.img_url = img_url
 
-treasures = [
-    Treasure('Gold Nugget', 500.00, 'gold', "Curly's Creek, NM",'/static/images/nugget.png'),
-    Treasure("Fool's Gold", 0, 'pyrite', "Fool's Falls, CO",'/static/images/fools-gold.png'),
-    Treasure('Coffee Can', 20.00, 'tin', 'Aacme, CA','/static/images/coffee-can.png'),
-]
+#treasures = [
+#    Treasure('Gold Nugget', 500.00, 'gold', "Curly's Creek, NM",'/static/images/nugget.png'),
+#    Treasure("Fool's Gold", 0, 'pyrite', "Fool's Falls, CO",'/static/images/fools-gold.png'),
+#    Treasure('Coffee Can', 20.00, 'tin', 'Aacme, CA','/static/images/coffee-can.png'),
+#]
 
 locations = [
     Location("Fool's Falls, CO", 'Flash Floods', 0,
