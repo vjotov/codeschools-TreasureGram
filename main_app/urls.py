011e3256f477f52fea525d652953b8cd42aa47c4
Vladimir Jotov <vjotov@>
2016-11-27 23:26:13 +0700

    treasure detail page

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^home/$', views.home),
-    url(r'^$', views.index),
-    
+    url(r'^home/$', views.home, name = 'home'),
+    url(r'^$', views.index, name = 'index'),
+    url(r'^([1-9])+/$', views.datail, name = 'detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render
 from .models import Treasure, Location
 
+
 # Create your views here.
-def index(request):  
-	treasures = Treasure.objects.all()
-	return render(request, 'index.html', {'treasures': treasures})
+def index(request):
+    treasures = Treasure.objects.all()
+    return render(request, 'index.html', {'treasures': treasures})
+
 
 def home(request):
-	locations = Location.objects.all()
-	context = {'locations': locations}
-	return render(request, 'home.html', context)
-    
+    locations = Location.objects.all()
+    context = {'locations': locations}
+    return render(request, 'home.html', context)
 
 
+def datail(request, treasure_id):
+    treasure = Treasure.objects.get(id=treasure_id)
+    context = {'treasure': treasure}
+    return render(request, "detail.html", context)
