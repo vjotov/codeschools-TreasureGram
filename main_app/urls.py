eeb0b99d191998189e975a62342d6d2b60b5c262
VladimirJotov <vjoto@saritasa.com>
2016-11-28 14:24:27 +0700

    add treasure feature

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     url(r'^home/$', views.home, name = 'home'),
     url(r'^$', views.index, name = 'index'),
     url(r'^([1-9])+/$', views.datail, name = 'detail'),
+    url(r'^post_url/$', views.post_treasure, name = 'post_treasure'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from .models import Treasure, Location
+from .forms import TreasureForm
 
 
 # Create your views here.
 def index(request):
     treasures = Treasure.objects.all()
-    return render(request, 'index.html', {'treasures': treasures})
+    form = TreasureForm()
+    context = {'treasures': treasures, 'form':form}
+    return render(request, 'index.html', context)
 
 
 def home(request):
@@ -18,3 +22,14 @@ def datail(request, treasure_id):
     treasure = Treasure.objects.get(id=treasure_id)
     context = {'treasure': treasure}
     return render(request, "detail.html", context)
+
+def post_treasure(request):
+    form = TreasureForm(request.POST)
+    if form.is_valid():
+        treasure = Treasure(
+            name = form.cleaned_data['name'],
+            value = form.cleaned_data['value'],
+            location = form.cleaned_data['location'],
+            img_url = form.cleaned_data['img_url'])
+        treasure.save()
+    return HttpResponseRedirect('/')
